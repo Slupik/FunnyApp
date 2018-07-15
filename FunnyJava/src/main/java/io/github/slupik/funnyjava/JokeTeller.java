29a75361be878f95e66c959a75fc2a095cd2d2a1
Slupik <Slupik98@gmail.com>
2018-07-15 23:51:25 +0200

    feat: add jokes

--- FunnyJava/src/main/java/io/github/slupik/funnyjava/JokeTeller.java
+++ FunnyJava/src/main/java/io/github/slupik/funnyjava/JokeTeller.java
@@ -11,7 +11,14 @@ public class JokeTeller {
     private static final String[] JOKES = new String[]{
             "There are exactly two types of mathematical objects: trivial ones, and those which have not yet been proven.",
             "Q: What's the value of a contour integral around Western Europe? A: Zero, because all the Poles are in Eastern Europe.",
-            "I'm sorry, the number you have dialed is imaginary. Please rotate by 90 degrees and try again."
+            "I'm sorry, the number you have dialed is imaginary. Please rotate by 90 degrees and try again.",
+            "Q: What's purple and commutes? A: An Abelian grape.",
+            "Q: What sound does a drowning analytic number theorist make? A: log log log log...",
+            "Q: Why did the mathematician name his dog \"Cauchy?\" A: Because he left a residue at every pole.",
+            "Q: What's sour, yellow, and equivalent to the axiom of choice? A: Zorn's lemon.",
+            "Q: What's hot, chunky, and acts on a polygon? A: Dihedral soup.",
+            "Rrrrrring. Operator: I'm sorry, the number you have dialed is imaginary. Please multiply by i and dial again.",
+            "1 = 2 (for sufficiently large values of 1)"
     };
 
     public static String getRandomJoke(){
