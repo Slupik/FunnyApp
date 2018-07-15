631c27a892a4d7cd34bf2dc73e8d318725b99c6c
Slupik <Slupik98@gmail.com>
2018-07-15 20:55:48 +0200

    feat: add library telling jokes

---
+++ FunnyJava/src/main/java/io/github/slupik/funnyjava/JokeTeller.java
@@ -0,0 +1,14 @@
+/*
+ * Copyright (c) 2018. by Sebastian Witasik
+ * All rights reserved. No part of this application may be reproduced or be part of other software, without the prior written permission of the publisher. For permission requests, write to the author(WitasikSebastian@gmail.com).
+ */
+
+package io.github.slupik.funnyjava;
+
+public class JokeTeller {
+    private static final String[] JOKES = new String[]{
+            "There are exactly two types of mathematical objects: trivial ones, and those which have not yet been proven.",
+            "Q: What's the value of a contour integral around Western Europe? A: Zero, because all the Poles are in Eastern Europe.",
+            "I'm sorry, the number you have dialed is imaginary. Please rotate by 90 degrees and try again."
+    };
+}
