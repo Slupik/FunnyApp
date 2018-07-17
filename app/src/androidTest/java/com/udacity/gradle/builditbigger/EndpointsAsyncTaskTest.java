69fc50a63292190af7c5a44a7af605f09472077e
Slupik <Slupik98@gmail.com>
2018-07-17 03:19:26 +0200

    test: Improved checking of AsyncTask

--- app/src/androidTest/java/com/udacity/gradle/builditbigger/EndpointsAsyncTaskTest.java
+++ app/src/androidTest/java/com/udacity/gradle/builditbigger/EndpointsAsyncTaskTest.java
@@ -5,39 +5,37 @@
 
 package com.udacity.gradle.builditbigger;
 
-import android.os.Handler;
-import android.os.Looper;
+import android.content.Context;
+import android.support.test.InstrumentationRegistry;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
+import android.test.InstrumentationTestCase;
 
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import static android.support.test.espresso.Espresso.onView;
-import static android.support.test.espresso.action.ViewActions.click;
-import static android.support.test.espresso.assertion.ViewAssertions.matches;
-import static android.support.test.espresso.matcher.ViewMatchers.withId;
-import static android.support.test.espresso.matcher.ViewMatchers.withText;
-import static org.hamcrest.Matchers.not;
+import java.util.concurrent.CountDownLatch;
 
 @RunWith(AndroidJUnit4.class)
-public class EndpointsAsyncTaskTest {
+public class EndpointsAsyncTaskTest extends InstrumentationTestCase {
 
     @Rule
     public ActivityTestRule<MainActivity> activityTestRule = new ActivityTestRule<>(MainActivity.class);
 
     @Test
-    public void clickButton_checkJokeIsDisplayed() {
-        onView(withId(R.id.instructions_text_view)).perform(click());
-
-        Looper.prepare();
-        Handler handler = new Handler();
-        handler.postDelayed(new Runnable() {
+    public void checkTaskReturnsJoke() throws Throwable {
+        final CountDownLatch latch = new CountDownLatch(1);
+        Context context = InstrumentationRegistry.getContext();
+        EndpointsAsyncTask testTask = new EndpointsAsyncTask(context) {
             @Override
-            public void run() {
-                onView(withId(R.id.joke_content)).check(matches(not(withText(""))));
+            protected void onPostExecute(String result) {
+                assertNotNull(result);
+                assertTrue(result.length() > 0);
+                latch.countDown();
             }
-        }, 1000);
+        };
+        testTask.execute();
+        latch.await();
     }
 }
